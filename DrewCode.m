36eba210c412c77bbe3471815868886e93740c1e
Drew Gillie <DrewGillieVFX@gmail.com>
2017-05-08 02:10:30 -0400

    fixed the black pieces

--- DrewCode.m
+++ DrewCode.m
@@ -12,6 +12,19 @@ imshow(K);
 % This gets the white pieces
 W = J - I;
 imshow(W);
+
+%% cleaning up the black pieces image
+K2 = imadjust(K,[0.1 0.7],[]); 
+imshow(K2);
+
+% delete horizontal lines
+seh = strel('line', 5,90);
+k = imdilate(K2, seh);
+% delete verticle lines
+sev = strel('line', 5,0);
+kk = imdilate(k, sev);
+imshow(kk);
+K3 =kk;
 %% finding pieces
 
 % disk for erosion
@@ -21,26 +34,26 @@ seK = strel('ball',5,5);
 
 % % erosion
 gw = imerode(W, seW);
-gk = imerode(K, seK);
-% %imshow(g);
+gk = imerode(K3, seK);
+imshow(gk);
 
 % dilate
 gwd = imdilate(gw, seW);
 gkd = imdilate(gk, seK);
-%imshow(g);
+imshow(gkd);
 
 pw = imbinarize(gwd);
 pk = imbinarize(gkd);
-%imshow(p);
+imshow(pk);
 
 %pw = imfill(pw, 8, 'holes');
 %pk = imfill(pk, 8, 'holes');
 %imshow(p);
 
 % find all pieces in image 
-cw = bwlabel(pw,4);
-ck = bwlabel(pk,4);
-%imshow(c);
+cw = bwlabel(pw);
+ck = bwlabel(pk);
+imshow(ck);
 
 [rw, colw] = find(cw==4);
 rcw = [rw colw];
@@ -59,9 +72,11 @@ imshow(mk);
 [cw, whitePieces] = bwlabel(cw);
 [ck, blackPieces] = bwlabel(ck);
 
+% temporary fix
+% blackPieces2 = (blackPieces / 4);
 %% displaying total White and Black pieces 
-disp(fprintf('White pieces is: %s', whitePieces));
-disp(fprintf('Black pieces is: %s', blackPieces));
+disp(sprintf('White pieces is: %d', whitePieces));
+disp(sprintf('Black pieces is: %d', blackPieces));
 %%
 % show the original image
 imshow(J);
