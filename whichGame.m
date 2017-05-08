16c6e4fef4bd650fbb4b594fa48ab8786f39a637
Drew Gillie <DrewGillieVFX@gmail.com>
2017-05-08 00:29:04 -0400

    update 2.

---
+++ whichGame.m
@@ -0,0 +1,11 @@
+%% whichGame
+
+% if pieces have cross in the middle
+% after differencer, then this game is go
+% This gets the black pieces
+K = I - J;
+imshow(K);
+
+% This gets the white pieces
+W = J - I;
+imshow(W);
