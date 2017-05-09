ad1ec22f6e4448590083d85ecf5a6963929ca4e1
Drew Gillie <DrewGillieVFX@gmail.com>
2017-05-09 00:52:20 -0400

    threshold edge

--- getFrame.m
+++ getFrame.m
@@ -1,28 +1,43 @@
+%%
+addpath('webcam/');
+
+files = dir('webcam/**');
+warning('off','all');
+warning('query','all');
+%disp(files);
+
+%%
+for file = files'
+     disp(file.name);
+     %imshow(strcat(file.folder,'/',file.name));
+     %O4 = imread(strcat(file.folder,'/',file.name));
+     O4 = imread(file.name);
+     a = rgb2gray(O4);
+     %imshow(a);
+     %AndrewCode;
+     pause(.5);
+end
+
+
+
+
+
+
+
+
+
+%%
+cam = webcam;
 %% snapshot
-vid = videoinput('winvideo'); 
+vid = videoinput('macVideo'); 
 img = getsnapshot(vid);
-I = img;
+imshow(img);
 %%
-imaqInfo = imaqhwinfo
-imaqInfo.InstalledAdaptors
-hwInfo = imaqhwinfo('winvideo')
-
-
-
-
+clear('cam');
 %%
-% Access an image acquisition device.
-vidobj = videoinput('dt', 1, 'RS170')
-% Access the device's video sources that can be used for acquisition.
-sources = vidobj.Source
+vid = videoinput('macvideo', 1, 'YCbCr422_1280x720');
+src = getselectedsource(vid);
+start(vid);
 
-vidobj.SelectedSourceName = 'VID2')
 
-% Notice that the corresponding video source has been selected.
-sources
-
-selectedsrc = getselectedsource(vidobj)
-
-% List the video source object's properties and their current values.
-get(selectedsrc)
 
